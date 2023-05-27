b7c0802b89e602e584c86fa1a16e57e015214896
lenghanyue515 <106796524+lenghanyue515@users.noreply.github.com>
2023-05-27 21:21:46 +0800

    Update Nicegram.js

--- Rewrite/Nicegram.js
+++ Rewrite/Nicegram.js
@@ -1,10 +1,8 @@
-/*
-脚本功能：解锁Nicegram会员
-脚本作者：R·E
-支持版本：商店最新 1.0.7
-更新时间：2022.09.23
-问题反馈：https://t.me/Functional_Store_Hub
-使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，禁止转载与贩卖！⚠️⚠️⚠️
-*/
+#!name=Nicegram会员
+#!desc=解锁Nicegram会员(2022.07.15)
+#!openUrl=https://github.com/I-am-R-E/Functional-Store-Hub/tree/Master/Nicegram
+#!author=@R·E https://t.me/Functional_Store_Hub
 
-const a=typeof $task!='undefined';const b=typeof $httpClient!='undefined';if(b){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'response':{'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'}});}else if(a){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':'HTTP/1.1\x20200\x20OK','headers':{'Content-Type':'application/json'},'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}else{console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}
+^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ url script-echo-response https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Nicegram/Script/Nicegram.js
+
+hostname = restore-access.indream.app
